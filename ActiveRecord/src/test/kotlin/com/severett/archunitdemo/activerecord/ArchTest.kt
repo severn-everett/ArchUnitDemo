ff017d46496d757d4fc20a252c9071beb1acbb41
Severn Everett <severn.everett@gmail.com>
2023-03-30 20:49:17 +0200

    Added Arch Unit tests for ActiveRecord project

--- ActiveRecord/src/test/kotlin/com/severett/archunitdemo/activerecord/ArchTest.kt
+++ ActiveRecord/src/test/kotlin/com/severett/archunitdemo/activerecord/ArchTest.kt
@@ -2,25 +2,84 @@ package com.severett.archunitdemo.activerecord
 
 import com.tngtech.archunit.base.DescribedPredicate
 import com.tngtech.archunit.core.domain.JavaClass
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods
-import org.springframework.stereotype.Controller
+import jakarta.persistence.Entity
+import org.junit.jupiter.api.assertAll
+import org.springframework.data.repository.Repository
+import org.springframework.web.bind.annotation.RestController
 import kotlin.test.Test
 
+private const val REPO_PACKAGE = "com.severett.archunitdemo.activerecord.repo"
+private const val DOMAIN_PACKAGE = "com.severett.archunitdemo.activerecord.model.domain"
+private const val DTO_PACKAGE = "com.severett.archunitdemo.activerecord.model.dto"
+
 class ArchTest {
+
+    private val activeRecordClasses = ClassFileImporter().importPackages("com.severett.archunitdemo.activerecord")
+
     @Test
-    fun aTest() {
+    fun repositoryInheritance() {
+        val repoInheritanceRule = classes()
+            .that()
+            .resideInAPackage(REPO_PACKAGE)
+            .should()
+            .beInterfaces()
+            .andShould()
+            .beAssignableTo(Repository::class.java)
+        val onlyInRepoRule = classes()
+            .that()
+            .resideOutsideOfPackage(REPO_PACKAGE)
+            .should()
+            .notBeAssignableTo(Repository::class.java)
+
+        assertAll(
+            { repoInheritanceRule.check(activeRecordClasses) },
+            { onlyInRepoRule.check(activeRecordClasses) },
+        )
+    }
+
+    @Test
+    fun noDomainLeakage() {
         val dtoPredicate = object : DescribedPredicate<JavaClass>("be either a standard collection or a DTO") {
             override fun test(t: JavaClass): Boolean {
-                return true
+                return when {
+                    t.packageName == DTO_PACKAGE -> true
+                    t.packageName == "java.lang" && t.isAssignableTo("void") -> true
+                    t.isAssignableTo(Collection::class.java) -> true
+                    else -> false
+                }
             }
         }
-        methods()
+
+        val dtoAnnotationRule = classes()
             .that()
-            .areDeclaredInClassesThat()
-            .areAnnotatedWith(Controller::class.java)
-            .and()
+            .resideInAPackage(DTO_PACKAGE)
+            .should()
+            .notBeAnnotatedWith(Entity::class.java)
+
+        val domainAnnotationRule = classes()
+            .that()
+            .resideInAPackage(DOMAIN_PACKAGE)
+            .should()
+            .beAnnotatedWith(Entity::class.java)
+            .orShould()
+            .beEnums()
+
+        val returnTypeRule = methods()
+            .that()
             .arePublic()
+            .and()
+            .areDeclaredInClassesThat()
+            .areAnnotatedWith(RestController::class.java)
             .should()
             .haveRawReturnType(dtoPredicate)
+
+        assertAll(
+            { dtoAnnotationRule.check(activeRecordClasses) },
+            { domainAnnotationRule.check(activeRecordClasses) },
+            { returnTypeRule.check(activeRecordClasses) },
+        )
     }
 }
