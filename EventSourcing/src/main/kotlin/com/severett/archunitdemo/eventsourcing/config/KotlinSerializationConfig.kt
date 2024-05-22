a26e4f6336f8ab88b666470d557ade992b700171
Severn Everett <severn.everett@gmail.com>
2024-05-22 09:56:11 +0200

    Fixed serialization configuration in EventSourcing demo

--- EventSourcing/src/main/kotlin/com/severett/archunitdemo/eventsourcing/config/KotlinSerializationConfig.kt
+++ EventSourcing/src/main/kotlin/com/severett/archunitdemo/eventsourcing/config/KotlinSerializationConfig.kt
@@ -3,16 +3,13 @@ package com.severett.archunitdemo.eventsourcing.config
 import kotlinx.serialization.json.Json
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.json.KotlinSerializationJsonDecoder
-import org.springframework.http.codec.json.KotlinSerializationJsonEncoder
+import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
 
 @Configuration
 class KotlinSerializationConfig {
-    private val json = Json { ignoreUnknownKeys = true }
-
     @Bean
-    fun encoder() = KotlinSerializationJsonEncoder(json)
-
-    @Bean
-    fun decoder() = KotlinSerializationJsonDecoder(json)
+    fun configBean(): KotlinSerializationJsonHttpMessageConverter {
+        val jsonConfig = Json { ignoreUnknownKeys = true }
+        return KotlinSerializationJsonHttpMessageConverter(jsonConfig)
+    }
 }
