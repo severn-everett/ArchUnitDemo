e6cda75dd905ed880b07e4c0f8a9fee00a19fde3
Severn Everett <severn.everett@gmail.com>
2024-05-24 11:49:15 +0200

    Upgraded Spring Boot to 3.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = "2.0.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.allopen") version kotlinVersion
-    id("org.springframework.boot") version "3.2.5"
+    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
 }
 
